539a171238cc7a1db25260014eee4a7ea963a7b8
joannatko <joannako@dhcp-v120-230.mobile.uci.edu>
2017-10-02 20:58:56 -0700

    made a hello world component

--- src/index.js
+++ src/index.js
@@ -2,5 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+//first componenet
+const App = () => { // function declaration in Modern JS
 
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <p>I love saying hello world </p>
+    </div>
+  ) //wrap in single parent element
+  // return () is a JSX thing, not JS
+}
+// links component to html
+ReactDOM.render(<App />, document.getElementById('root')); // component name
 registerServiceWorker();
